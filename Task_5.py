195a64672c24b382503763a02deab39015832bdb
Andrey Shchepetov <shchepetov2012@yandex.ru>
2023-10-25 15:53:43 +0300

    Task_24

--- Task_5.py
+++ Task_5.py
@@ -7,22 +7,29 @@ class Animal:
     def __int__(
             self,
             animal_type: str,
-            eat: Literal['травоядное', 'хищное'],
+            feeds: Literal['травоядное', 'хищное'],
             animal_makes_a_sound: str,
     ):
         self.animal_type = animal_type
-        self.eat = eat
+        self.feeds = feeds
         self.animal_makes_a_sound = animal_makes_a_sound
 
     def __str__(self) -> str:
-        return f'Это {self.eat} животное {self.animal_type}!'
+        return f'Это {self.feeds} животное {self.animal_type}!'
 
     def sound(self) -> str:
         return f'{self.animal_type} издает звуки {self.animal_makes_a_sound}!'
 
+    def feeds(self) -> str:
+        return f'{self.animal_type} кушает {self.feeds}!'
 
-cat = Animal('кот', 'мяукает', 'хищное')
-elephant = Animal('слон', 'трубит', 'травоядное')
+
+cat = Animal('кот', 'мяукает', 'хищное', 'кушает', 'мясо')
 
 print(cat, cat.sound())
+print(cat, cat.feeds())
+
+elephant = Animal('слон', 'трубит', 'травоядное', 'кушает', 'бананы')
+
 print(elephant, elephant.sound())
+print(elephant, elephant.feeds())
